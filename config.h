cd2a42d9b0e5ee8d43b840de2cf93bfd2e5ae1a9
Breno Brandão <lrabbt@gmail.com>
2020-04-21 22:02:17 -0300

    Get patched hack font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:style=Regular:size=11:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:style=Regular:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
